77f506c9ef8f2d391d1f2d37409125f024074025
matheus souza <ruivorobado@gmail.com>
2021-02-09 15:12:09 -0300

    script completo

--- assets/javascript/animaScroll.js
+++ assets/javascript/animaScroll.js
@@ -1,3 +1,19 @@
-$(() => {
+$(function() {
+    $(window).scroll(function() {
+        $('.sessao').each(function() {
+            var windowOffY = $(window).scrollTop()
+            var windowHeight = $(window).height()
+            var elOffy = $(this).offset().top
 
+            if (elOffy < (windowOffY + windowHeight) &&
+                elOffy + $(this).height() > windowOffY) {
+                $('a').css('border-bottom', '0')
+                var target = $(this).attr('target')
+                $('.' + target).css('border-bottom', '1px solid white')
+
+
+                return
+            }
+        })
+    })
 })
